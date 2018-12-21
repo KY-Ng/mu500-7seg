e9709d99d48dfcc2efbc06ed3797f9e8ddcdf7b5
Kenji Hisazumi <hisazumi@gmail.com>
2018-12-21 13:08:48 +0900

    initial commit

---
+++ mu500_7seg.py
@@ -0,0 +1,73 @@
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+
+class QtLED(QLabel):
+    def __init__(self):
+        super().__init__("  ")
+        self.pix = QPixmap(20, 10)
+        self.off()
+
+    def setFillColor(self, color):
+        self.pix.fill(color)
+        self.setPixmap(self.pix)
+
+    def on(self):
+        self.setFillColor(Qt.yellow)
+
+    def off(self):
+        self.setFillColor(Qt.black)
+
+
+class MU500_7SEG(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        BoxLay = QVBoxLayout()
+        self.setLayout(BoxLay)
+
+        self.init_leds(BoxLay)
+        self.init_7seg(BoxLay)
+
+        Stool = QSlider(Qt.Horizontal, self)
+        BoxLay.addWidget(Stool)
+        Stool.valueChanged.connect(self.w7segs[0].display)
+
+        self.setGeometry(500, 500, 300, 500)
+        self.setWindowTitle('EventTest')
+        self.show()
+
+        self.leds[10].on()
+        self.w7segs[0].display("0100")
+
+
+    def init_7seg(self, layout):
+        self.w7segs = []
+        for i in range(4):
+            hboxlay = QHBoxLayout()
+            layout.addLayout(hboxlay)
+            for j in range(4):
+                w7seg = QLCDNumber(self)
+                w7seg.setHexMode()
+                w7seg.display("0000")
+                hboxlay.addWidget(w7seg)
+                self.w7segs.append(w7seg)
+
+    def init_leds(self, layout):
+        self.leds = []
+        for j in range(4):
+            hboxlay = QHBoxLayout()
+            layout.addLayout(hboxlay)
+            for i in range(16):
+                led = QtLED()
+                hboxlay.addWidget(led)
+                self.leds.append(led)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = MU500_7SEG()
+    sys.exit(app.exec_())
